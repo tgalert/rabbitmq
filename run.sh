707687aec187363b0d99da01d029b19a7f15d0de
Daniel Weibel <danielmweibel@gmail.com>
2018-12-08 18:38:40 -0500

    Move from Go program to Bash script
    Use the Go implementation of the previous commit as a reference of how
    to implement these tasks in Go.

--- run.sh
+++ run.sh
@@ -1,3 +1,81 @@
 #!/bin/bash
 
-go run src/* -k ~/.kube/config
+set -e
+
+# Coloured output
+COL='\033[1;32m'
+NOC='\033[0m'
+echocln() {
+  echo -e "$COL$*$NOC"
+}
+echoc() {
+  echo -e -n "$COL$*$NOC"
+}
+
+#------------------------------------------------------------------------------#
+# Deploy RabbitMQ
+#------------------------------------------------------------------------------#
+
+echocln "> Deploying RabbitMQ"
+kubectl create -f rabbitmq.yml
+
+#------------------------------------------------------------------------------#
+# Wait for service endpoint
+#------------------------------------------------------------------------------#
+
+# Endpoint is either DNS name or IP address. This script works for both cases.
+# JSONPath:
+#   - https://kubernetes.io/docs/reference/kubectl/jsonpath/
+# LoadBalancerIngress array:
+#   - https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.12/#loadbalanceringress-v1-core
+echoc "> Waiting for service endpoint to be published"
+while [[ -z "$endpoint" ]]; do
+  sleep 1
+  echoc .
+  endpoint=$(kubectl get service rabbitmq -o jsonpath='{.status.loadBalancer.ingress[0].*}')
+done
+echo -e "\nSuccess: $endpoint"
+
+#------------------------------------------------------------------------------#
+# TODO: store service endpoint somewhere > AWS Systems Manager Parameter Store
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
+# Wait for endpoint DNS resolution
+#------------------------------------------------------------------------------#
+
+echoc "> Waiting for endpoint DNS propagation (may take a few minutes)"
+until host "$endpoint" >/dev/null; do
+  sleep 1
+  echoc .
+done
+echo -e "\nSuccess"
+
+#------------------------------------------------------------------------------#
+# Replace admin user
+#------------------------------------------------------------------------------#
+
+username=admin
+password=$(cat /dev/urandom | LC_ALL=C tr -d -c 'a-zA-Z0-9' | head -c 32)
+echocln "> Replacing default administrator {guest:guest} with {$username:**********}"
+
+# Base URL of the RabbitMQ Management HTTP REST API
+base_url="http://$endpoint:15672/api"
+
+curl \
+  -X PUT \
+  -u guest:guest \
+  -H "Content-Type: application/json" \
+  -d "{\"password\":\"$password\",\"tags\":\"administrator\"}" \
+  "$base_url/users/$username"
+
+curl \
+  -X DELETE \
+  -u guest:guest \
+  "$base_url/users/guest"
+
+echo "Success"
+
+#------------------------------------------------------------------------------#
+# Store credentials of new admin user in AWS Secrets Manager
+#------------------------------------------------------------------------------#
